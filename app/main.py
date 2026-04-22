2abf96638315b966338937615c3d32bceaec3aa1
Yaroslav Vynogradov <vinogradovarik6@gmail.com>
2026-04-22 18:20:18 +0200

    implement py-mutable-immutable

--- app/main.py
+++ app/main.py
@@ -17,8 +17,9 @@ marks = {
 collection_of_coins = {1, 2, 25}
 
 sorted_variables = {
-    "mutable": [marks,collection_of_coins, my_favourite_films],
-    "immutable": [lucky_number, one_is_a_prime_number, pi, profile_info, name]
-
-
+    "mutable": [marks, collection_of_coins, my_favourite_films],
+    "immutable": [lucky_number, pi, one_is_a_prime_number, name, profile_info]
 }
+
+
+
